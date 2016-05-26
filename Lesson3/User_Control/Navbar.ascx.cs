652d0d682d4607a11e93b34b245ac7256b05ea0c
vandit18 <vanditkothari@gmail.com>
2016-05-26 14:11:08 -0400

    add Navbar Page

---
+++ Lesson3/User_Control/Navbar.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/**
+ @author: Vandit Kothari
+    @date: May 26,2016
+*/
+namespace Lesson3
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+        /*
+         * This method adds a css class of "active" to list items related
+         * to navigation links of each page
+         * 
+         * @method SetAcitvePage
+         * @return {void}
+         * */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
