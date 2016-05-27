8919f9d450f803e2c80775ee2c65518090d1187e
vandit18 <vanditkothari@gmail.com>
2016-05-26 23:20:19 -0400

    Adding Jumbotron Control and Navbar as User_control

--- Lesson3/Default.aspx
+++ Lesson3/Default.aspx
@@ -1,9 +1,10 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lesson3.Default" %>
 
+<%@ Register Src="~/User_Control/Jumbotron.ascx" TagPrefix="bs3" TagName="Jumbotron" %>
+
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div class="container">
-        <h1>
-            Welcome
-        </h1>
-    </div>
+    <bs3:Jumbotron runat="server" ID="Jumbotron" >
+
+    </bs3:Jumbotron>
 </asp:Content>
--- Lesson3/User_Control/Jumbotron.ascx.designer.cs
+++ Lesson3/User_Control/Jumbotron.ascx.designer.cs
@@ -7,18 +7,18 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lesson3 {
+namespace Lesson3.User_Control {
     
     
-    public partial class Default {
+    public partial class Jumbotron {
         
         /// <summary>
-        /// Jumbotron control.
+        /// JumbotronH1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::Lesson3.User_Control.Jumbotron Jumbotron;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl JumbotronH1;
     }
 }
